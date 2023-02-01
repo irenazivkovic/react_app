e24991f8f246eafbd9f7424acd28ac6b2a45b71c
Irena Živković <zivkovicirena3@gmail.com>
2023-02-01 14:33:35 +0100

    Fixed scss issues

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+
+import github from '../../assets/svg/github-logo.svg';
+import linekedin from '../../assets/svg/linkedin-logo.svg';
+import gmail from '../../assets/svg/gmail-logo.svg';
+
 import './about.scss';
 
 const About= () => {
@@ -14,7 +19,8 @@ const About= () => {
           If you would like to reach out to me, please do so why any of the following link.
         </p>
         <div className="contact">
-          
+          <a href="https://github.com/irenazivkovic" id='github' target='_blank'><img src={github} /></a>
+          <a href="https://www.linkedin.com/in/irena-živković-46593422a/" id='linkedin' target='_blank'><img src={linekedin} /></a>
         </div>
       </div>
      <Footer />
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,7 +5,7 @@ import './header.scss';
 const Header = () => {
   return(
     <div className="header">
-      <h1>Weather App</h1>
+      <Link to='/'><h1>Weather App</h1></Link>
       <div className="options">
         <Link to='/about'>about</Link>
         <Link to='/weather'>weather</Link>
